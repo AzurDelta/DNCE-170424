3b2fe0f1ebd74bf590a2a7cdeab12d70ae725b3a
AzurDelta <Rotar.zahar31@gmail.com>
2024-04-22 13:07:08 +0300

    common task done

--- DNCE-170424-G/Program.cs
+++ DNCE-170424-G/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Goodbye, World!");
+            var len = args.Length;
+            List<string> rearranged = new List<string>();
+            
+            for (int i = 0; i < len; i++)
+            {
+                if (i == 0 || args[i] != args[i - 1])
+                     rearranged.Add(args[i]);
+            }
+            
+            rearranged.ForEach(e => Console.Write($"{e}; "));
+            
         }
+        // аргументы передаются как аргументы "dotnet run"
     }
 }
