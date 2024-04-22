e85f689fc5452ca7947e60b59c8a44a39020dc07
AzurDelta <Rotar.zahar31@gmail.com>
2024-04-22 12:15:01 +0300

    initial commit

---
+++ DNCE-170424-G/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DNCE_170424_G
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodbye, World!");
+        }
+    }
+}
