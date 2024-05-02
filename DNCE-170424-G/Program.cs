633f7a7ef688ac941a69a757f6eb67d94144c508
AzurDelta <Rotar.zahar31@gmail.com>
2024-05-02 12:00:06 +0300

    input type correction

--- DNCE-170424-G/Program.cs
+++ DNCE-170424-G/Program.cs
@@ -1,26 +1,37 @@
-﻿namespace DNCE_170424_G
+﻿using System.Dynamic;
+
+namespace DNCE_170424_G
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            DoublesDistinct(args);
-            
+            string[] strs = new[] { "str","str", "str3", "notastring","notastring", "maybeastring" };
+            int[] ints = new[] { 5, 3, 6, 7, 9,9,3,3,4 };
+            var strsList = new List<string>() { "str","str", "str3", "notastring","notastring", "maybeastring" };
+            DoublesDistinct(strs);
+            DoublesDistinct(ints);
+            DoublesDistinct(strsList);
         }
-        // аргументы передаются как аргументы "dotnet run"
 
-        static void DoublesDistinct(string[] args)
+        static void DoublesDistinct<T>(T[] args)
         {
             var len = args.Length;
-            List<string> rearranged = new List<string>();
             
             for (int i = 0; i < len; i++)
             {
-                if (i == 0 || args[i] != args[i - 1])
-                    rearranged.Add(args[i]);
-            }
-            
-            rearranged.ForEach(e => Console.Write($"{e}; "));   
+                if (i == 0 || !args[i].Equals(args[i - 1]))
+                    Console.WriteLine(args[i]);
+            }   
         }
+        
+        static void DoublesDistinct<T>(List<T> args)
+        {
+            var len = args.Count;
+            
+            args = args.Where((arg, i) => i == 0 || !arg.Equals(args[i - 1])).ToList(); 
+            args.ForEach(x => Console.WriteLine(x));
+        }
+
     }
 }
